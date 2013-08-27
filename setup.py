a2645d8fe5115203ab81541ac42848add23a6401
Thomas Van Doren <thomas.vandoren@gmail.com>
2013-08-26 19:31:17 -0700

    Update setup.py based on extension dev doc.
    http://flask.pocoo.org/docs/extensiondev/

--- setup.py
+++ setup.py
@@ -1,27 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+Flask-BugzScout
+===============
+
+`BugzScout <https://pypi.python.org/pypi/bugzscout>`_ extension for Flask.
+"""
+
 from setuptools import find_packages, setup
 
 setup(
-    name            ='Flask-BugzScout',
-    version         =open('VERSION', 'r').read(),
-    description     ='BugzScout extension for Flask.',
-    long_description=open('README.rst').read(),
-    author          ='Thomas Van Doren',
-    author_email    ='thomas@thomasvandoren.com',
-    maintainer      ='Thomas Van Doren',
-    maintainer_email='thomas@thomasvandoren.com',
-    url             ='https://github.com/thomasvandoren/Flask-BugzScout',
-    keywords        =['BugzScout', 'Flask', 'FogBugz'],
-    license         ='BSD',
-    packages        =find_packages(exclude=('test',)),
-    install_requires=[
+    name                ='Flask-BugzScout',
+    version             =open('VERSION', 'r').read(),
+    description         ='BugzScout extension for Flask.',
+    long_description    =open('README.rst').read(),
+    author              ='Thomas Van Doren',
+    author_email        ='thomas@thomasvandoren.com',
+    maintainer          ='Thomas Van Doren',
+    maintainer_email    ='thomas@thomasvandoren.com',
+    url                 ='https://github.com/thomasvandoren/Flask-BugzScout',
+    keywords            =['BugzScout', 'Flask', 'FogBugz'],
+    license             ='BSD',
+    packages            =find_packages(exclude=('test',))
+    zip_safe            =False,
+    include_package_data=True,
+    platforms           ='any',
+    install_requires    =[
         'bugzscout',
         'Flask',
         ],
-    classifiers     =[
+    classifiers         =[
         'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
@@ -32,9 +43,7 @@ setup(
         # 'Programming Language :: Python :: 3.2',
         # 'Programming Language :: Python :: 3.3',
         # 'Programming Language :: Python :: Implementation :: PyPy',
-        ],
-    entry_points    ={
-        'console_scripts': [
-            ]
-        }
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
     )
