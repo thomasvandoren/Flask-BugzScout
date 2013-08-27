63baf7a4f7b01626156e2898c713bca284e389fe
Thomas Van Doren <thomas.vandoren@gmail.com>
2013-08-26 19:35:56 -0700

    Add placeholder BugzScout class.

---
+++ flask_bugzscout/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Flask-BugzScout"""
+
+import bugzscout
+
+
+class BugzScout(object):
+    """Recording Flask and HTTP errors to FogBugz via BugzScout."""
+
