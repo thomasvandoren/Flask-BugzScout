6d7ef8902c8605f887d497bf58f9ffadfbac9dd6
Thomas Van Doren <thomas.vandoren@gmail.com>
2013-09-12 15:36:42 -0700

    Add error handlers.

--- flask_bugzscout/__init__.py
+++ flask_bugzscout/__init__.py
@@ -3,6 +3,9 @@
 
 """Flask-BugzScout"""
 
+import flask
+import functools
+
 
 class BugzScout(object):
     """Recording Flask and HTTP errors to FogBugz via BugzScout."""
@@ -35,11 +38,51 @@ class BugzScout(object):
 
         app.config.setdefault('BUGZSCOUT_HTTP_CODES', set(xrange(400, 418)))
 
-        # TODO: Think about this... (thomas, 2013-09-12)
-        if not app.testing:
-            self.url = app.config['BUGZSCOUT_URL']
-            self.user = app.config['BUGZSCOUT_USER']
-            self.project = app.config['BUGZSCOUT_PROJECT']
-            self.area = app.config['BUGZSCOUT_AREA']
+        self.url = app.config['BUGZSCOUT_URL']
+        self.user = app.config['BUGZSCOUT_USER']
+        self.project = app.config['BUGZSCOUT_PROJECT']
+        self.area = app.config['BUGZSCOUT_AREA']
 
-            app.extensions['bugzscout'] = self
+        app.handle_exception = self._get_exception_handler(app)
+        app.handle_http_exception = self.get_http_exception_handler(app)
+
+        app.extensions['bugzscout'] = self
+
+    def _get_exception_handler(self, app):
+        """Create exception handler that wraps app exception handler.
+
+        :type app: Flask app
+        :arg app: Flask app to report exceptions to BugzScout
+        """
+        handle_exception = app.handle_exception
+
+        @functools.wraps(handle_exception)
+        def wrapper(exception):
+            self._report_error(flask._request_ctx_stack.top)
+            return handle_exception(exception)
+
+        return wrapper
+
+    def _get_http_exception_handler(self, app):
+        """Create HTTP exception handler that wraps app HTTP exception handler.
+
+        :type app: Flask app
+        :arg app: Flask app to report HTTP exceptions to BugzScout
+        """
+        handle_http_exception = app.handle_http_exception
+
+        @functools.wraps(handle_http_exception)
+        def wrapper(exception):
+            if exception.code in app.config['BUGZSCOUT_HTTP_CODES']:
+                self._report_error(flask._request_ctx_stack.top)
+            return handle_http_exception(exception)
+
+        return wrapper
+
+    def _report_error(self, top_of_stack):
+        """FIXME!
+
+        :type top_of_stack: FIXME
+        :arg top_of_stack: FIXME
+        """
+        pass
